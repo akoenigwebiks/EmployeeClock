899405d050d8ea20a5f0d0622ae261aecd540e70
Avi E Koenig <darkmift@gmail.com>
2024-06-25 18:38:12 +0300

    added pw change form

---
+++ EmployeeClock/PasswordChangeForm.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace EmployeeClock
+{
+    partial class PasswordChangeForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox_id = new System.Windows.Forms.TextBox();
+            this.label_insertId = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label_insertOldPassword = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label_insertNewPassword = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label_insertConfirmNewPassword = new System.Windows.Forms.Label();
+            this.button_submit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox_id
+            // 
+            this.textBox_id.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox_id.Location = new System.Drawing.Point(328, 50);
+            this.textBox_id.Margin = new System.Windows.Forms.Padding(6);
+            this.textBox_id.Name = "textBox_id";
+            this.textBox_id.Size = new System.Drawing.Size(248, 38);
+            this.textBox_id.TabIndex = 3;
+            // 
+            // label_insertId
+            // 
+            this.label_insertId.AutoSize = true;
+            this.label_insertId.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_insertId.Location = new System.Drawing.Point(83, 57);
+            this.label_insertId.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label_insertId.Name = "label_insertId";
+            this.label_insertId.Size = new System.Drawing.Size(142, 31);
+            this.label_insertId.TabIndex = 2;
+            this.label_insertId.Text = "תעודת זהות";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(328, 122);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(6);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(248, 38);
+            this.textBox1.TabIndex = 5;
+            // 
+            // label_insertOldPassword
+            // 
+            this.label_insertOldPassword.AutoSize = true;
+            this.label_insertOldPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_insertOldPassword.Location = new System.Drawing.Point(83, 129);
+            this.label_insertOldPassword.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label_insertOldPassword.Name = "label_insertOldPassword";
+            this.label_insertOldPassword.Size = new System.Drawing.Size(146, 31);
+            this.label_insertOldPassword.TabIndex = 4;
+            this.label_insertOldPassword.Text = "סיסמא ישנה";
+            // 
+            // textBox3
+            // 
+            this.textBox3.BackColor = System.Drawing.Color.Cornsilk;
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox3.Location = new System.Drawing.Point(328, 196);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(6);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(248, 38);
+            this.textBox3.TabIndex = 9;
+            // 
+            // label_insertNewPassword
+            // 
+            this.label_insertNewPassword.AutoSize = true;
+            this.label_insertNewPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_insertNewPassword.Location = new System.Drawing.Point(83, 199);
+            this.label_insertNewPassword.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label_insertNewPassword.Name = "label_insertNewPassword";
+            this.label_insertNewPassword.Size = new System.Drawing.Size(158, 31);
+            this.label_insertNewPassword.TabIndex = 8;
+            this.label_insertNewPassword.Text = "סיסמא חדשה";
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.Cornsilk;
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(328, 272);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(6);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(248, 38);
+            this.textBox2.TabIndex = 11;
+            // 
+            // label_insertConfirmNewPassword
+            // 
+            this.label_insertConfirmNewPassword.AutoSize = true;
+            this.label_insertConfirmNewPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_insertConfirmNewPassword.Location = new System.Drawing.Point(83, 275);
+            this.label_insertConfirmNewPassword.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label_insertConfirmNewPassword.Name = "label_insertConfirmNewPassword";
+            this.label_insertConfirmNewPassword.Size = new System.Drawing.Size(231, 31);
+            this.label_insertConfirmNewPassword.TabIndex = 12;
+            this.label_insertConfirmNewPassword.Text = "אישור סיסמא חדשה";
+            // 
+            // button_submit
+            // 
+            this.button_submit.BackColor = System.Drawing.SystemColors.Info;
+            this.button_submit.Location = new System.Drawing.Point(328, 345);
+            this.button_submit.Margin = new System.Windows.Forms.Padding(6);
+            this.button_submit.Name = "button_submit";
+            this.button_submit.Size = new System.Drawing.Size(248, 45);
+            this.button_submit.TabIndex = 13;
+            this.button_submit.Text = "ביצוע החלפת סיסמא";
+            this.button_submit.UseVisualStyleBackColor = false;
+            // 
+            // PasswordChangeForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(667, 454);
+            this.Controls.Add(this.button_submit);
+            this.Controls.Add(this.label_insertConfirmNewPassword);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label_insertNewPassword);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label_insertOldPassword);
+            this.Controls.Add(this.textBox_id);
+            this.Controls.Add(this.label_insertId);
+            this.Name = "PasswordChangeForm";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.RightToLeftLayout = true;
+            this.Text = "שעון נוכחות - החלפת סיסמא";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox_id;
+        private System.Windows.Forms.Label label_insertId;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label_insertOldPassword;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label_insertNewPassword;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label_insertConfirmNewPassword;
+        private System.Windows.Forms.Button button_submit;
+    }
+}
