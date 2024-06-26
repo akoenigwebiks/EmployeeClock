565c8ad942ac2a6cfdeacc00e815c064bb740fea
Avi E Koenig <darkmift@gmail.com>
2024-06-26 16:23:25 +0300

    added util

---
+++ EmployeeClock/Utils/ConverterUtils.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Data;
+
+namespace EmployeeClock.Utils
+{
+    internal class ConverterUtils
+    {
+        public static string GetByKey(DataRow row, string key)
+        {
+            return row[key].ToString();
+        }
+    }
+}
