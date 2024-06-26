b300e4a4200e19d9eb066c0233cb973282559a19
Avi E Koenig <darkmift@gmail.com>
2024-06-26 09:34:36 +0300

    code conventions,summary added

--- EmployeeClock/LoginForm.cs
+++ EmployeeClock/LoginForm.cs
@@ -8,10 +8,13 @@ namespace EmployeeClock
     public partial class LoginForm : Form
     {
         DatabaseManager _databaseManager;
+        ClockForm _clockForm;
+
         public LoginForm(DatabaseManager databaseManager)
         {
             _databaseManager = databaseManager;
             InitializeComponent();
+            _clockForm = new ClockForm(); // Initialize once and reuse
         }
 
         private void textBox_id_TextChanged(object sender, EventArgs e)
@@ -20,7 +23,18 @@ namespace EmployeeClock
 
         }
 
-        private void button_submit_Click(object sender, EventArgs e)
+        /// <summary>
+        /// Handles the click event of the submit button on the login form.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">An EventArgs that contains no event data.</param>
+        /// <remarks>
+        /// This method retrieves the username and password entered by the user, checks if either field is empty,
+        /// and displays a message if so. If both fields are filled, it validates the login credentials against the database.
+        /// Upon successful validation, a success message is shown, the clock form is displayed, and the login form is closed.
+        /// If validation fails, an error message is displayed.
+        /// </remarks>
+        private void Button_submit_Click(object sender, EventArgs e)
         {
             string username = textBox_id.Text;
             string password = textBox_password.Text;
@@ -31,11 +45,13 @@ namespace EmployeeClock
                 MessageBox.Show("נא למלא את כל השדות");
                 return;
             }
-            bool isValidLogin = checkLogin(username, password);
+            bool isValidLogin = CheckLogin(username, password);
             MessageBox.Show(isValidLogin ? "התחברת בהצלחה" : "שם משתמש או סיסמא שגויים");
+            _clockForm.Show();
+            this.Close();
         }
 
-        private bool checkLogin(string username, string password)
+        private bool CheckLogin(string username, string password)
         {
             string query = $@"SELECT * FROM Employees 
                                 INNER JOIN Passwords 
